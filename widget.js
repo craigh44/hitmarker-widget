7ffc9217d001fbde55d6e6cfd190d4e10fd3bb81
Craig Horsborough <craighorsborough@gmail.com>
2017-05-11 11:13:58 +0100

    removed jquery

--- widget.js
+++ widget.js
@@ -1,11 +1,29 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
   var element = document.getElementById("hitmarker-jobs-feed");
   var spinner = document.createElement("img");
   spinner.src = 'https://i1.wp.com/cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif'
   element.appendChild(spinner);
-  $.get("http://localhost:8888/hitmarker/jobs.json", function(data) {
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://localhost:8888/hitmarker/jobs.json');
+  xhr.send(null);
+
+  xhr.onreadystatechange = function () {
+    var DONE = 4;
+    var OK = 200;
+
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK) {
+        var jobs = JSON.parse(xhr.responseText).data;
+
+        appendJobs(jobs);
+      } else {
+        // console.log('Error: ' + xhr.status); // An error occurred during the request.
+      }
+    }
+  }
+  function appendJobs (jobs) {
     spinner.style.display = 'none';
-    var jobs = data.data;
     for (var i = 0; i < jobs.length; i++) {
       var jobWrapper = document.createElement("div");
       var jobLink = document.createElement("a");
@@ -15,5 +33,5 @@ $(document).ready(function() {
       jobWrapper.appendChild(jobLink);
       element.appendChild(jobWrapper);
     }
-  });
+  }
 });
