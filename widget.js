942f79b2b1543dfc7d5de8c455cdcbb4e53ab9dd
Craig Horsborough <craighorsborough@gmail.com>
2017-05-15 15:22:40 +0100

    all the things

--- widget.js
+++ widget.js
@@ -1,47 +1,105 @@
 (function (){
-  var element = document.getElementById("hitmarker-jobs-feed");
-  var spinner = document.createElement("img");
-  spinner.src = 'https://i1.wp.com/cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif'
-  element.appendChild(spinner);
+    var element = document.getElementById("hitmarker-jobs-feed");
+    var spinner = document.createElement("img");
+    spinner.src = 'https://i1.wp.com/cdnjs.cloudflare.com/ajax/libs/galleriffic/2.0.1/css/loader.gif';
+    element.appendChild(spinner);
 
+    var normalizeStyles = document.createElement("link");
+    var hitmarkerStyles = document.createElement("link");
+    hitmarkerStyles.rel = "stylesheet";
+    hitmarkerStyles.type = "text/css";
+    hitmarkerStyles.href = "https://rawgit.com/craigh44/hitmarker-widget/master/hitmarkerwidget.css";
+    hitmarkerStyles.media = "all";
+    document.getElementsByTagName("head")[0].appendChild(hitmarkerStyles);
 
-  var styleTag = document.createElement("link");
-  styleTag.rel = "stylesheet";
-  styleTag.type = "text/css";
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:8888/hitmarker/jobs.json');
+    xhr.send(null);
 
-  styleTag.href = "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css";
-  styleTag.media = "all";
+    xhr.onreadystatechange = function () {
+      var DONE = 4;
+      var OK = 200;
 
-  document.getElementsByTagName("head")[0].appendChild(styleTag)
+      if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+          var jobs = JSON.parse(xhr.responseText).data;
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://localhost:8888/hitmarker/jobs.json');
-  xhr.send(null);
-
-  xhr.onreadystatechange = function () {
-    var DONE = 4;
-    var OK = 200;
-
-    if (xhr.readyState === DONE) {
-      if (xhr.status === OK) {
-        var jobs = JSON.parse(xhr.responseText).data;
-
-        appendJobs(jobs);
-      } else {
-        // console.log('Error: ' + xhr.status); // An error occurred during the request.
+          appendJobs(jobs);
+        } else {
+          console.log('Error: ' + xhr.status); // An error occurred during the request.
+        }
       }
     }
-  }
-  function appendJobs (jobs) {
-    spinner.style.display = 'none';
-    for (var i = 0; i < jobs.length; i++) {
-      var jobWrapper = document.createElement("div");
-      var jobLink = document.createElement("a");
-      jobLink.target = "_blank";
-      jobLink.href = jobs[i].url;
-      jobLink.innerHTML = jobs[i].title;
-      jobWrapper.appendChild(jobLink);
-      element.appendChild(jobWrapper);
+      function appendJobs (jobs) {
+        spinner.style.display = 'none';
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://raw.githubusercontent.com/craigh44/hitmarker-widget/master/index.html');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function () {
+          var DONE = 4;
+          var OK = 200;
+
+          if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+
+              var xmlString = xhr.responseText,
+              parser = new DOMParser(),
+              doc = parser.parseFromString(xmlString, "text/html");
+
+              element.appendChild(doc.documentElement);
+
+              addJobs();
+
+            } else {
+              console.log('Error: ' + xhr.status); // An error occurred during the request.
+            }
+          }
+        }
+
+        function addJobs () {
+
+
+        for (var i = 0; i < jobs.length; i++) {
+
+          //create link wrapper
+          var jobWrapper = document.createElement("a");
+          jobWrapper.target = "_blank";
+          jobWrapper.href = jobs[i].url;
+          jobWrapper.classList.add('job');
+
+          var logoWrapper = document.createElement("div");
+          logoWrapper.classList.add('logo');
+
+          var logoImg = document.createElement("img");
+          logoImg.src = jobs[i].imageUrl;
+          logoImg.width = '32';
+          logoImg.height = '32';
+
+          logoWrapper.appendChild(logoImg);
+          jobWrapper.appendChild(logoWrapper);
+
+          var jobInfoWrapper = document.createElement("div");
+          jobInfoWrapper.classList.add('info');
+
+          var jobCompany = document.createElement("div");
+          jobCompany.classList.add('company');
+
+          var jobTitle = document.createElement("div");
+          jobTitle.classList.add('role');
+
+          jobCompany.innerHTML = jobs[i].company;
+          jobTitle.innerHTML = jobs[i].title;
+
+          jobInfoWrapper.appendChild(jobCompany);
+          jobInfoWrapper.appendChild(jobTitle);
+
+          jobWrapper.appendChild(jobInfoWrapper);
+
+          var jobsWrapper = document.getElementById('inner-wrapper');
+          jobsWrapper.appendChild(jobWrapper);
+        }
+      }
     }
-  }
-})();
+  })();
